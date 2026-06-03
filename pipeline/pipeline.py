144440b5d0989ee382f10c022167f9b24ea7c472
Saùl Romero <61168379+sromero9485@users.noreply.github.com>
2026-06-03 21:53:35 +0000

    update_branch

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,3 +4,10 @@ print("arguments", sys.argv)
 day = int(sys.argv[1])
 print(f"Running pipeline for day {day}")
 
+import pandas as pd
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
